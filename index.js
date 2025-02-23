2d7f94682b71a0318967a5bb65bc68b23c33035c
Paul Landers <paul@thelanders.family>
2025-02-22 21:46:34 -0500

    Fix log again

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function main() {
 
 async function listDriveFolder() {
   const result = await drive.files.list({ q: `'${folder}' in parents and trashed = false` });
-  actions.info(`List Api result: ${result}`);
+  actions.info(`List Api result: ${JSON.stringify(result)}`);
   const files = result.files;
   // TODO:  return a Map
   return files.map((f) => ({ name: f.name, id: f.id }));
